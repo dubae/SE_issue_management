463cd07e42878e68aa1c339065ac9b632571b9c7
rocknroll <gon1397@naver.com>
2024-05-28 04:06:10 +0900

    Dummy commmit

--- src/main/java/com/codingrecipe/member/controller/HomeController.java
+++ src/main/java/com/codingrecipe/member/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.codingrecipe.member.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import lombok.RequiredArgsConstructor;
 
 
